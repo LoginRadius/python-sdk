04b0b8578d538f54bf331f6015625cad3df85e9b
Shivam Mishra <shivamkumar6370@gmail.com>
2020-10-02 11:09:59 +0530

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-# read the contents of your README file
+# read the contents of README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
